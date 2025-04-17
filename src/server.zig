30a334ffb050bdb51f8c776f9517352b9b4b09fb
gagem-sigsev <erebus.sigsev@gmail.com>
2025-04-16 23:10:16 -0500

    dev commit | needs refactoring

--- src/server.zig
+++ src/server.zig
@@ -1,39 +1,40 @@
 const std = @import("std");
 const print = std.debug.print;
 const Thread = std.Thread;
+const Client = @import("client.zig").Client;
+
 const Controller = @import("controller.zig");
 
 pub const ZecterServer = struct {
     server_address: std.net.Address,
     connections: std.ArrayList(std.net.Server.Connection),
     streams: std.ArrayList(std.net.Stream),
+    active_users: std.ArrayList([]const u8),
+    clients: std.ArrayList(*Client),
     mutex: std.Thread.Mutex,
 
-    var list_offset: i8 = -1; // index offset
 
     const Self = @This();
     pub fn newZecter(allocator: *std.mem.Allocator) !*ZecterServer {
         const zecter: *ZecterServer = try allocator.create(ZecterServer);
-        zecter.server_address = try parse_address("127.0.0.1", 8080);
+        zecter.server_address = try parseAddress("127.0.0.1", 8080);
         zecter.connections = std.ArrayList(std.net.Server.Connection).init(allocator.*);
         zecter.streams = std.ArrayList(std.net.Stream).init(allocator.*);
+        zecter.active_users = std.ArrayList([]const u8).init(allocator.*);
+        zecter.clients = std.ArrayList(*Client).init(allocator.*);
         zecter.mutex = std.Thread.Mutex{};
         return zecter;
     }
 
-    fn parse_address(ip: []const u8, port: u16) !std.net.Address {
+    fn parseAddress(ip: []const u8, port: u16) !std.net.Address {
         const address = try std.net.Address.parseIp4(ip, port);
         return address;
     }
 
-    // TODO: IMPLEMENT SERVER FUNCTIONALITY IN SINGLE THREAD FOR STARTERS
     pub fn startServer(self: *Self) !void {
         // create a listener LISTENER IS SERVER TYPE
         const listener_opts = std.net.Address.ListenOptions{ .force_nonblocking = false, .kernel_backlog = 20, .reuse_address = true, .reuse_port = true };
         const listener = try std.net.Address.listen(self.server_address, listener_opts);
-        // self.handleClient(@constCast(&listener)) catch |err| {
-        // print("Error handling client: {any}\n", .{err});
-        //};
 
         var accept_thread = try Thread.spawn(.{}, acceptClient, .{ self, @constCast(&listener) });
         var console_thread = try Thread.spawn(.{}, consoleListener, .{self});
@@ -43,27 +44,53 @@ pub const ZecterServer = struct {
     }
 
     fn acceptClient(self: *Self, listener: *std.net.Server) !void {
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const allocator = gpa.allocator();
         while (true) {
 
             // accept a client
             const conn = try listener.accept();
-
+            
+            // Lock the mutex to protect shared data
             self.mutex.lock();
+            
+            // Append the new client connection into the connections array list
             self.connections.append(conn) catch |err| {
                 print("Error Adding Client to Connections List: {any}\n", .{err});
             };
-            list_offset += 1;
+           
+            // obtain the stream from the connection 
             const stream = conn.stream;
             self.streams.append(stream) catch |err| {
                 print("Error adding stream to streams: {any}\n", .{err});
             };
-            std.Thread.sleep(1_000_000_000);
-            self.mutex.unlock();
 
-            //var thread = try Thread.spawn(.{}, handleClient, .{ self, conn.stream });
-            //thread.join();
+            // allocate memory for the username from the client 
+            print("Allocating buffer for client username...\n", .{});
+            const user_name = try allocator.alloc(u8, 1024);
+            
+            // send the username prompt to the client 
+            print("Buffer allocated with 1024 bytes, Prompting for client input...\n", .{});
+            const bytes_sent = try stream.write("Username->: ");
+            
+            // receive the username from the client 
+            print("Sent {d} bytes to client...\n", .{bytes_sent});
+            const bytes_read = try stream.read(user_name);
+            
+            // resize the memory to prevent memory waste
+            print("Client input received, resizing buffer to {d} bytes...\nClient username: {s}\n", .{ bytes_read, user_name[0 .. bytes_read - 1] });
+            const resize_status = allocator.resize(user_name, bytes_read);
+            _ = resize_status;
+            print("Buffer resized to bytes_read - 1 = {d} bytes. Null terminator eliminated...\nCreating client structure...\n", .{bytes_read - 1});
+            
+            // append the username to active users array list 
+            self.active_users.append(user_name[0 .. bytes_read - 1]) catch |err| {
+                print("Unable to append user to active users list: {any}\n", .{err});
+            };
+            
+            // unlock the mutex to allow shared resources to be available for other threads
+            self.mutex.unlock();
         }
-        listener.deinit();
     }
 
     fn consoleListener(self: *Self) !void {
